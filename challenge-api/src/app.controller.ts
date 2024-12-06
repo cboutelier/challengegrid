4df3a2a481f62cd153efb7c3ec100a2263082eb9
cboutelier <ced.boutelier@gmail.Com>
2024-12-06 19:44:41 +0100

    Add task service on front

--- challenge-api/src/app.controller.ts
+++ challenge-api/src/app.controller.ts
@@ -1,9 +1,42 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {  Controller,
+  Get,
+  Post,
+  Body,
+  Delete,
+  Param,
+  HttpException,
+  HttpStatus, } from '@nestjs/common';
+import { AppService, Tasks } from './app.service';
 
-@Controller()
+@Controller('api/todos')
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getTodos(): Tasks[] {
+    try {
+      return this.appService.getTasks();
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+  @Post()
+  createTodo(@Body() { name }: Tasks): Tasks[] {
+    try {
+      return this.appService.createTask(name);
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+  @Delete(':id')
+  deleteTodo(@Param('id') id: number): Tasks[] {
+    try {
+      return this.appService.deleteTask(id);
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
 
   @Get()
   getHello(): string {
